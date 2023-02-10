41d4cbf4d934be20976dab3ec41a4ee35da910f8
Tteve <asdq2780@gmail.com>
2023-02-10 21:06:26 +0600

    add orders

--- main/urls.py
+++ main/urls.py
@@ -1,24 +1,13 @@
-"""main URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.conf.urls.static import static
+from rest_framework.routers import SimpleRouter
 from django.conf.urls.static import static
 from django.conf import settings
+from rest_framework import permissions
 from rest_framework.routers import SimpleRouter
-
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 from category.views import CategoryViewSet
 from product.views import ProductViewSet
 
@@ -26,15 +15,6 @@ router = SimpleRouter()
 router.register('categories', CategoryViewSet)
 router.register('products', ProductViewSet)
 
-from django.urls import path, include, re_path
-from django.conf.urls.static import static
-from django.conf import settings
-from rest_framework import permissions
-from rest_framework.routers import SimpleRouter
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-
-router = SimpleRouter()
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -56,6 +36,7 @@ urlpatterns = [
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
     path('api/v1/accounts/', include('account.urls')),
+    path('api/v1/orders/', include('order.urls')),
     path('api/v1/', include(router.urls)),
 ]
 
