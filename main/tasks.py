33adb66988662be3cc601b906c14dba70a5e2470
pafosnyyHop <odecik30@gmail.com>
2023-02-11 19:25:41 +0600

    added celery tasks

--- main/tasks.py
+++ main/tasks.py
@@ -4,7 +4,7 @@ from account.send_email import send_reset_email, send_confirmation_email
 
 @app.task
 def send_confirm_email_task(user, code):
-    send_confirmation_email(user, code)
+    send_confirmation_email(user=user, code=code)
 
 
 @app.task
