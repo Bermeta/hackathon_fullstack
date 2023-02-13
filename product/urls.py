cfcf8b7e8fd191c600c172508c8428ae790a8779
Tteve <asdq2780@gmail.com>
2023-02-13 20:18:31 +0600

    delete posts

--- product/urls.py
+++ product/urls.py
@@ -4,7 +4,6 @@ from . import views
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
-router.register('posts', views.ProductViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
