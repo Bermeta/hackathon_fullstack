240eb34b1f4ffe072a9dafee621534dc5dc734e4
bermeta <bermetzarlyk@gmail.com>
2023-02-20 20:42:18 +0600

    added admin register favorites and Product Images

--- product/admin.py
+++ product/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from product.models import Product, Like
+from product.models import Product, Like, Favorites, ProductImages
 
 # Register your models here.
 admin.site.register(Product)
 admin.site.register(Like)
+admin.site.register(Favorites)
+admin.site.register(ProductImages)
